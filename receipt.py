71329eaad09a621b88cf8c2de5ed2ce5e7c9e1cd
cprincec <chukwuprincec@gmail.com>
2022-03-11 14:11:34 +0100

    push

--- receipt.py
+++ receipt.py
@@ -3,6 +3,7 @@ import datetime
 #import csv module for reading 
 #the csv files.
 import csv
+from fileinput import filename
 
 PRODUCT_KEY_INDEX = 0
 PRODUCT_NAME_INDEX = 1
@@ -12,31 +13,44 @@ REQUESTED_QUANTITY_INDEX = 1
 
 
 def main():
-    #call the read_dict fucntion and save its reuturned value as producs_dict.
-    products_dict = read_dict("products.csv", PRODUCT_KEY_INDEX)
+    products_file = "products.csv"
+    try:
+        #call the read_dict fucntion and save its reuturned value as producs_dict.
+        products_dict = read_dict(products_file, PRODUCT_KEY_INDEX)
+    
+    #handle file not found error for products.csv file    
+    except FileNotFoundError as not_found_error:
+        print(f"Error: {products_file} was not fond in your directory.")
+        print("Choose another file.")
+    
+    #Handle permission error for products.csv file.
+    except PermissionError as no_permission:
+        print(f"Error: You do not have permission to access \
+            {products_file}")
+        print("Contact your admin or choose another file.")
 
+    order_file = "request.csv"
     print()
     # print("All Products")
     # print(products_dict)
 
     #print name of store.
     print("Inkom Emporium Stores")
+    try:
+        #open and read the reciept.csv file
+        with open(order_file, "rt") as request_file:
+            subtotal = 0
+            total_products = 0
+            request_file = csv.reader(request_file)
+            next(request_file)
+            print()
 
-    #open and read the reciept.csv file
-    with open("products.csv", "rt") as request_file:
-        subtotal = 0
-        total_products = 0
-        request_file = csv.reader(request_file)
-        next(request_file)
-        print()
+            #read rows of request.
+            for line in request_file:
+                product_num = line[PRODUCT_KEY_INDEX]
 
-        #read rows of request.
-        for line in request_file:
-            product_num = line[PRODUCT_KEY_INDEX]
-
-            #use the product number of each row in the request.csv
-            #to find the corresponding items in the products dictionary.
-            if product_num in products_dict:
+                #use the product number of each row in the request.csv
+                #to find the corresponding items in the products dictionary.
                 product_num_value = products_dict[product_num]
                 product_name = product_num_value[PRODUCT_NAME_INDEX]
                 product_price = float(product_num_value[PRODUCT_PRICE_INDEX])
@@ -45,25 +59,36 @@ def main():
                 subtotal += (product_price * quantity)
 
                 print(f"{product_name}: {quantity} @ {product_price}")
-            else:
-                print(f"{product_name.capitalize()} is out of stock.")
+                
+            sales_tax = compute_sales_tax(subtotal, 6)
+            total_amount = compute_total(subtotal, sales_tax)
+            current_date_and_time = datetime.datetime.now()
+            date = datetime.date.today()
+            
 
-        sales_tax = compute_sales_tax(subtotal, 6)
-        total_amount = compute_total(subtotal, sales_tax)
-        current_date_and_time = datetime.datetime.now()
-        date = datetime.date.today()
-        
+            print()
+            print(f"Number of items: {total_products}")
+            print(f"Subtotal: ${subtotal:.2f}")
+            print(f"Sales tax: {sales_tax:.2f}")
+            print(f"Total: ${total_amount:.2f}")
+            print()
+            print("Thanks for your patronage!!!")
+            print(f"{current_date_and_time:%A %I:%M %p} {date}")
+            print()
 
-        print()
-        print(f"Number of items: {total_products}")
-        print(f"Subtotal: ${subtotal:.2f}")
-        print(f"Sales tax: {sales_tax:.2f}")
-        print(f"Total: ${total_amount:.2f}")
-        print()
-        print("Thanks for your patronage!!!")
-        print(f"{current_date_and_time:%A %I:%M %p} {date}")
-        print()
-        
+    #handle file not found error for request.csv file.        
+    except FileNotFoundError as not_found_error:
+        print(f"Error: {order_file} was not fond in your directory.")
+        print("Choose another file.") 
+
+    #Handle permission error for request.csv file.
+    except PermissionError as no_permission:
+        print(f"Error: You do not have permission to access \
+            {order_file}")
+        print("Contact your admin or choose another file.")  
+
+    except KeyError as key_error:
+        print(f"This product is out of stock.")     
 
 
 
@@ -89,7 +114,7 @@ def read_dict(filename, key_column_index):
         #save it as reader.
         reader = csv.reader(products_file)
 
-        #skip the headings row of the file.
+        #skip the headings row                                                                                                                                                                                                                                                                 of the file.
         next(reader)
 
         #loop through each row of the csv file
