9aeb636639dafdf065e6da812deb83c5da5ed7d7
cprincec <chukwuprincec@gmail.com>
2022-04-03 16:31:38 +0100

    commit

--- cbt.py
+++ cbt.py
@@ -1,4 +1,5 @@
 import csv
+import tkinter as tk
 
 """This is a Computer-Based Testing/Exam Program. 
 This program is made with in the context of an objective exam for a high school called National Secondary School."""
@@ -10,23 +11,48 @@ print("Class: SS1")
 print("Time: 1hr")
 print("Section A: Objectives")
 
-filename = "question_bank.csv"
-score = 0
-
-name = input("\n\nEnter your Name: ")
-reg_no = input("Enter your Registration Number: ")
-start = input("Start Exam (y/n): ")
-
 def main():
-    students = create_student_account(name, reg_no, score)
-    question_bank = read_quest_file(filename)
+    #Create the tk root object.
+    root = tk.Tk()
+    #Create the main window.
+    frm_main = tk.Frame(root, bg="blue")
+    frm_main.master.title("COMPUTER BASED EXAMINATION PROGRAM")
+    frm_main.pack()
 
-    print(students)
-    print(question_bank)
+    root.mainloop()
 
+    # filename = "question_bank.csv"
+    # score = 0
+    # choices = ["a", "b", "c", "d"]
+    # name = input("\n\nEnter your Name: ")
+    # reg_no = input("Enter your Registration Number: ")
+    # start = input("Start Exam (y/n): ")
+
+    # students = create_student_account(name, reg_no, score)
+    # question_bank = read_quest_file(filename)
     
+    # for question in question_bank:
 
+    #     quest =  question["question"]
+    #     a = question["option_a"]
+    #     b = question["option_b"]            
+    #     c = question["option_c"]
+    #     d = question["option_d"]
+    #     answer = question["answer"]
 
+    #     print(f"{quest} \n\nA. {a} \nB. {b} \nC. {c} \nD. {d}")
+
+    #     choice =  input(">>> ") 
+    #     while choice.lower() not in choices:
+    #         print("Invalid option.") 
+    #         print("Enter A or B or C or D.")
+    #         choice =  input(">>> ")      
+    #     if choice == answer:
+    #         score += 1
+    #     else:
+    #         pass
+    # print(f"{name}, You scored {score} out of 11.") 
+              
 
 def create_student_account(name, reg_no, score):
     """Create a dictionary that stores a student's name, score with reg number as the key.
@@ -46,18 +72,19 @@ def read_quest_file(filename):
     bank = []
     
     with open(filename, "rt") as question_bank:
-        question_bank = csv.reader(question_bank)
-        next(question_bank)
-        for row in question_bank:
+        reader = csv.reader(question_bank)
+        next(reader)
+        for row in reader:
             quest = row[0]
             option_a = row[1]
             option_b = row[2]
             option_c = row[3]
             option_d = row[4]
             answer = row[5]
-            bank.append({"question": quest, "option_a": option_a, "option_b": option_b,\
-                 "option_c": option_c, "option_d": option_d, "answer": answer})
+            bank.append({"question": quest, "option_a": option_a, "option_b": option_b, "option_c": option_c, "option_d": option_d, "answer": answer})
     return bank
+
+
 
 
 
